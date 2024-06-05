6300914b9062a36488f3ef65a44027be8b0dfab6
gabrielseiko <gabriel-s98@hotmail.com>
2024-06-04 20:39:44 -0500

    Proyecto Base

---
+++ LibreriaBackend/src/main/java/com/edu/pe/cibertec/LibreriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.pe.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaBackendApplication.class, args);
+	}
+
+}
---
+++ LibreriaBackend/src/test/java/com/edu/pe/cibertec/LibreriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.pe.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
