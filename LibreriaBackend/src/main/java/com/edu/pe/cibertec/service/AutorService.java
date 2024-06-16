79a20e506767ccbf6216b570bbc017093b7400ab
KimEsquivel14 <kimberly.esquivel14@gmail.com>
2024-06-16 00:56:41 -0500

    Avance grupo de Autor

--- LibreriaBackend/src/main/java/com/edu/pe/cibertec/service/AutorService.java
+++ LibreriaBackend/src/main/java/com/edu/pe/cibertec/service/AutorService.java
@@ -1,43 +1,14 @@
-package com.edu.pe.cibertec.service;
+package com.edu.pe.cibertec.repositoryService;
 
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.edu.pe.cibertec.entity.Autor;
-import com.edu.pe.cibertec.repository.IAutorRepository;
-import com.edu.pe.cibertec.repositoryService.IAutorService;
 
-@Service
-public class AutorService implements IAutorService {
+public interface IAutorService {
 
-	@Autowired
-	private IAutorRepository data;
-	
-	@Override
-	public List<Autor> ListadoAutores() {
-		return (List<Autor>) data.findAll();
-	}
-
-	@Override
-	public Optional<Autor> BuscarAutor(int id) {
-		return data.findById(id);
-	}
-
-	@Override
-	public int Grabar(Autor objA) {
-		int rpta = 0;
-		Autor a = data.save(objA);
-		if (!a.equals(null))
-			rpta = 1;
-		return rpta;
-	}
-
-	@Override
-	public void Suprimir(int id) {
-		data.deleteById(id);
-	}
-
+	public List<Autor> ListadoAutores();
+	public Optional<Autor> BuscarAutor(int id);
+	public int Grabar(Autor objA);
+	public void Suprimir(int id);
 }
---
+++ LibreriaBackend/src/main/java/com/edu/pe/cibertec/service/AutorServiceImpl.java
@@ -0,0 +1,43 @@
+package com.edu.pe.cibertec.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edu.pe.cibertec.entity.Autor;
+import com.edu.pe.cibertec.repository.IAutorRepository;
+import com.edu.pe.cibertec.repositoryService.IAutorService;
+
+@Service
+public class AutorService implements IAutorService {
+
+	@Autowired
+	private IAutorRepository data;
+	
+	@Override
+	public List<Autor> ListadoAutores() {
+		return (List<Autor>) data.findAll();
+	}
+
+	@Override
+	public Optional<Autor> BuscarAutor(int id) {
+		return data.findById(id);
+	}
+
+	@Override
+	public int Grabar(Autor objA) {
+		int rpta = 0;
+		Autor a = data.save(objA);
+		if (!a.equals(null))
+			rpta = 1;
+		return rpta;
+	}
+
+	@Override
+	public void Suprimir(int id) {
+		data.deleteById(id);
+	}
+
+}
