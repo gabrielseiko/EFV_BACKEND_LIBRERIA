411acd6f0c782eb8a8feae79927ff9f1710e8a95
DaniGuajardo <danign20@hotmail.com>
2024-06-16 01:44:37 -0500

    correccion errores 2.0

--- LibreriaBackend/src/main/java/com/edu/pe/cibertec/repository/LibroReservaRepository.java
+++ LibreriaBackend/src/main/java/com/edu/pe/cibertec/repository/LibroReservaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.edu.pe.cibertec.entity.LibroReserva;
 
 public interface LibroReservaRepository extends JpaRepository<LibroReserva, Integer>{
-	@Query("select l from tbl_libroreserva l where l.idLibro = ?1")
+	@Query("select l from LibroReserva l where l.libro.idLibro = ?1")
 	public abstract List<LibroReserva> listaLibroReservaPorLibroIgual(int idLibro);
 
 }
